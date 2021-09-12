5f85bcdd4be727db3920d0739da8f228f5b645e7
Thu <everly@arevolabs.com>
2021-09-12 20:38:34 +0700

    config port to deploy

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use((error, req, res, next) => {
 mongoose
   .connect('mongodb+srv://everly:xanhduong@elearning.whpyx.mongodb.net/elearning?retryWrites=true&w=majority')
   .then(() => {
-    app.listen(5000);
+    app.listen(process.env.PORT || 5000);
   })
   .catch(err => {
     console.log(err);
